879c8d4b3518c5e448e4a25635ccfacfec40b0bb
ethan <yaonie084@gmail.com>
2015-03-23 14:25:14 +0800

    misc

--- tests/test.js
+++ tests/test.js
@@ -22,7 +22,7 @@ describe("Routes", function() {
         var mockRes = {
           send: function(res) {
             res.should.exist;
-            res.should.eql({foo: 'success123'});
+            res.should.eql({foo: 'success'});
           }
         };
         pages.test(mockReq, mockRes)
